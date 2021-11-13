c0f136b5690657b2f9acca40657e8207095d85e4
Hamda <88338758+hamdahamda@users.noreply.github.com>
2021-11-13 15:00:08 +0700

    perbaikan-animasi-javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,56 +31,55 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // scroll reveal animation
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '80px',
+  origin: "top",
+  distance: "80px",
   duration: 300,
-  reset: true
-
-})
+  reset: true,
+});
 
 // scroll home
-sr.reveal('.home__title', {})
-sr.reveal('.button', {
-  delay: 300
-})
-sr.reveal('.home__image', {
-  delay: 300
-})
-sr.reveal('.home__social-icon', {
-  interva3: 300
-})
+sr.reveal(".home__title", {});
+sr.reveal(".button", {
+  delay: 200,
+});
+sr.reveal(".home__image", {
+  delay: 200,
+});
+sr.reveal(".home__social-icon", {
+  interva3: 200,
+});
 
 // scroll5about
-sr.reveal('.about__img', {})
-sr.reveal('.about__subtitle', {
-  delay: 500
-})
-sr.reveal('.about__text', {
-  delay: 500
-})
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", {
+  delay: 300,
+});
+sr.reveal(".about__text", {
+  delay: 300,
+});
 
 // scroll skills
 
-sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__text', {
-  delay: 300
-})
-sr.reveal('.skills__data', {
-  interval: 300
-})
-sr.reveal('.skills__img', {
-  delay: 500
-})
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {
+  delay: 200,
+});
+sr.reveal(".skills__data", {
+  interval: 200,
+});
+sr.reveal(".skills__img", {
+  delay: 300,
+});
 
 // scroll work
-sr.reveal('.work__img', {
-  interval: 300
-})
+sr.reveal(".work__img", {
+  interval: 200,
+});
 
 // scroll contact
-sr.reveal('.contact__input', {
-  interval: 300
-})
+sr.reveal(".contact__input", {
+  interval: 200,
+});
 
 // typing animation script
 
@@ -88,5 +87,5 @@ var typed = new Typed(".typing", {
   strings: ["a Developer", "a Designer", "a Freelancer"],
   typedSpeed: 100,
   backSpeed: 100,
-  loop: true
+  loop: true,
 });
